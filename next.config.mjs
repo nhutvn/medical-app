015d8be688589dd3a93b1576ecccc9c44f02a5c6
Lê Minh Nhựt <nhutlm@runsystem.net>
2024-04-11 13:20:34 +0700

    update build main project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: '/404.html',
+				destination: '/',
+			},
+		];
+	},
+	distDir: 'dist',
+	output: 'export',
+};
 
 export default nextConfig;
